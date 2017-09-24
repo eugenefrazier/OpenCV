cfffaf7d8bbee197ce29d8d11bf4b5a4191f9986
eugenefrazier <eugene_frazier96@hotmail.com>
2017-09-24 17:35:36 +0800

    Add files via upload

---
+++ FeatureMatching/FeatureMatching.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+video_capture = cv2.VideoCapture(0)
+
+# Here you have to add your picture 
+img2 = cv2.imread('img2.jpg',0)
+
+orb = cv2.ORB_create()
+
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+matches = bf.match(des1,des2)
+matches = sorted(matches, key = lambda x:x.distance)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
+plt.imshow(img3)
+plt.show()
